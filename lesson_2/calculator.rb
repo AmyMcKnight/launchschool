7a0f27764aa6da8f22c26a3cb6992bbf094b4447
Amy McKnihgt <simplyamym@gmail.com>
2016-02-16 03:35:15 -0500

    initial comit

---
+++ lesson_2/calculator.rb
@@ -0,0 +1,14 @@
+# ask the user for two numbers.
+# ask the user for an operation to perform
+# perform the opperation on the two numbers
+# out put the results
+
+# answer = Kernal.gets()
+# Kernal.puts(answer)
+
+Kernel.puts("Welcome to Calculator!")
+
+Kernel.puts("What's the first number?")
+number1 = Kernel.gets().chomp()
+
+Kernel.puts(number1)
